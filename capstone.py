7d415ae1ba12e6dff014ec3fc59272fb938e1e24
DmitryBachin <dmitry.bachin@yourapi.io>
2018-09-19 19:00:01 +0200

    partly break on functions

--- capstone.py
+++ capstone.py
@@ -16,22 +16,12 @@ def all_kind_of_value(data_set, variable):
     return kinds
 
 
-"""THE MAIN PART"""
-
-# the variables in the code book are written in lowercase, but actually they are in uppper
-# so need a little conversion
-variables = ['event_type', 'month_name', 'state']
-variables = list(map(str.upper, variables))
-
-data = pandas.read_csv('data_related/storm_event_data.csv', low_memory=False)
-
-
 def damage_property_int(row):  # creating the variable damage in int format to avoid 10K 12M 2B etc
     element = row['DAMAGE_PROPERTY']
     multiplier = {
-        'K': 1000,
-        'M': 1000*1000,
-        'B': 1000*1000*1000
+        'K': 10 ** 3,
+        'M': 10 ** 6,
+        'B': 10 ** 9
     }
     if type(element) is str:
         if element[-1] in multiplier:
@@ -46,21 +36,39 @@ def damage_property_int(row):  # creating the variable damage in int format to a
         return None
 
 
-data['damage_int'] = data.apply(lambda row: damage_property_int(row), axis=1)
+def modify_data_set(data_set):
+    data_set['damage_int'] = data_set.apply(lambda row: damage_property_int(row), axis=1)
+    return data_set
 
-# making a subset where we consider only wheather events for which damage is evalueted and bigger than zero
 
-data_with_damage = data[data['damage_int'] > 0]
+def output_distribution(data_set, used_variables):
+    print('The sample volume is %i' % len(data_with_damage))
+    print('')
+    for variable in used_variables:
+        c = data_set.groupby(variable).size()
+        print(c)
+        pt = c * 100 / len(data_set)
+        print(pt)
 
-print('The sample volume is %i' % len(data_with_damage))
-print('')
-# TODO: counting mean and start deviation for damage property
-# TODO: break on functions
-# TODO: make a plan
 
-# checking percantege and graph for explanatory variables (all of them are categorical)
-for variable in variables:
-    c = data_with_damage.groupby(variable).size()
-    print(c)
-    pt = c * 100 / len(data_with_damage)
-    print(pt)
+if __name__ == "__main__":
+    # the variables in the code book are written in lowercase, but actually they are in uppper
+    # so need a little conversion
+    variables = ['event_type', 'month_name', 'state']
+    variables = list(map(str.upper, variables))
+
+    data = pandas.read_csv('data_related/storm_event_data.csv', low_memory=False)
+    data = modify_data_set(data)
+
+    # making a subset where we consider only wheather events for which damage is evalueted and bigger than zero
+
+    data_with_damage = data[data['damage_int'] > 0]
+
+    output_distribution(data_with_damage, variables)
+    # TODO: counting mean and start deviation for damage property,
+
+    # TODO: break on functions,
+
+    # TODO: make a plan,
+
+    # checking percantege and graph for explanatory variables (all of them are categorical
