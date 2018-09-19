a7dfec1008c1bccf59ab524f26b67f8320793b5f
DmitryBachin <dmitry.bachin@yourapi.io>
2018-09-19 10:48:53 +0200

    add requirements file; check the existing code;

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Data-Analysis-Capstone
 The capstone from the course "Data Analysis and Interpretation" provided by Wesleyan University
+The code is written for the final project by Dmitry Bachin (dmitrybachin.com)
+The course is organized by Wesleyan University (USA) through Courseta.org
--- capstone.py
+++ capstone.py
@@ -2,63 +2,63 @@
 This is the code written for the final project by Dmitry Bachin (dmitrybachin.com)
 The course is organized by Wesleyan University (USA) through Courseta.org
 """
+import numpy
+import pandas
 
 """ FUNCTIONS FOR TESTING """
 
 
-def AllKindOfValue(dataset, Variable):
-    Kinds = []
-    for i in dataset[Variable]:
-        if i not in Kinds:
-            Kinds.append(i)
-    return Kinds
+def all_kind_of_value(data_set, variable):
+    kinds = []
+    for i in data_set[variable]:
+        if i not in kinds:
+            kinds.append(i)
+    return kinds
 
 
 """THE MAIN PART"""
 
-import numpy
-import pandas
-
 # the variables in the code book are written in lowercase, but actually they are in uppper
 # so need a little conversion
-Variables = ['event_type', 'month_name', 'state']
-Variables = list(map(str.upper, Variables))
+variables = ['event_type', 'month_name', 'state']
+variables = list(map(str.upper, variables))
 
 data = pandas.read_csv('data_related/storm_event_data.csv', low_memory=False)
 
 
 def damage_property_int(row):  # creating the variable damage in int format to avoid 10K 12M 2B etc
     element = row['DAMAGE_PROPERTY']
-
+    multiplier = {
+        'K': 1000,
+        'M': 1000*1000,
+        'B': 1000*1000*1000
+    }
     if type(element) is str:
-
-        if element[-1] == 'K':
-            return int(float(element[:-1]) * 1000)
-        elif element[-1] == 'M':
-            return int(float(element[:-1]) * 1000000)
+        if element[-1] in multiplier:
+            return int(float(element[:-1]) * multiplier[element[-1]])
         else:
-            return int(float(element[:-1]) * 1000000000)
+            try:
+                return int(element[:])
+            except ValueError:
+                print(f'The value {element} was not transformed properly')
+                return None
     else:
-        return 0  # later I plan to exclude both unknown and zero damage, so in this data management step
-        # it is possible to assign 0 to unknown damage
+        return None
 
 
 data['damage_int'] = data.apply(lambda row: damage_property_int(row), axis=1)
 
 # making a subset where we consider only wheather events for which damage is evalueted and bigger than zero
 
+data_with_damage = data[data['damage_int'] > 0]
 
-data = data[data['damage_int'] > 0]
-
-print('The sample volume is %i' % len(data))
+print('The sample volume is %i' % len(data_with_damage))
 print('')
-# counting mean and start deviation for damage property
+# TODO: counting mean and start deviation for damage property
 
 # checking percantege and graph for explanatory variables (all of them are categorical)
-for Variable in Variables:
-    c = data.groupby(Variable).size()
+for variable in variables:
+    c = data_with_damage.groupby(variable).size()
     print(c)
-    pt = c * 100 / len(data)
-    print('')
-
-
+    pt = c * 100 / len(data_with_damage)
+    print(pt)
