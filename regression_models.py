48720a0bd374afa368c57d89410898686e03ce8b
DmitryBachin <dmitry.bachin@yourapi.io>
2018-10-01 00:45:30 +0200

    regression finished and commented;

--- regression_models.py
+++ regression_models.py
@@ -8,15 +8,24 @@ import statsmodels.formula.api as smf
 
 
 def formula_creation(r_var, e_vars):
+    # creating the formula for ols
     return f"{r_var} ~ " + " + ".join(e_vars)
+
+
+def logistic_regression(q_explanatory, cat_explanatory, response, data_set):
+    # lreg = smf.logit(formula = formila, data = data_set).fit()
+    # print(lreg.summary())
+    pass
 
 
 def regression_modeling(q_explanatory, cat_explanatory, response, data_set):
     # modify categorical variables in one hot encoding
-    modified_data = recode_categorical_variables(data_set, cat_explanatory)
-    other_data = data_set[q_explanatory + response].copy()  # taking non modified variables
-    other_data = other_data.reset_index()  # reset indexing to concat the data frames properly
-    data_set = pd.concat([modified_data, other_data], axis=1)  # creating new data set where all categorical are binary
+    if cat_explanatory:
+        modified_data = recode_categorical_variables(data_set, cat_explanatory)
+        other_data = data_set[q_explanatory + response].copy()  # taking non modified variables
+        other_data = other_data.reset_index()  # reset indexing to concat the data frames properly
+        data_set = pd.concat([modified_data, other_data],
+                             axis=1)  # creating new data set where all categorical are binary
     explanatory_variables = [variable for variable in data_set.columns if variable not in response + ["index"]]
 
     # centering quantitative variables
@@ -37,7 +46,7 @@ if __name__ == "__main__":
     quantitative_response_variables = retrieve_q_response_variables()
     categorical_response_variables = retrieve_cat_response_variables()
 
-    categorical_explanatory_variables.remove("event_type")  # because it is correlating with cz_type
+    categorical_explanatory_variables.remove("event_type")  # too much categories
 
     # performing data management with data where property is damaged and evaluated clear
     data = primary_data_management(quantitative_explanatory_variables[:] + categorical_explanatory_variables[:],
@@ -47,9 +56,11 @@ if __name__ == "__main__":
     # performing regression analysis
     regression_modeling(quantitative_explanatory_variables[:], categorical_explanatory_variables[:],
                         quantitative_response_variables[:], data)
-
-    data = primary_data_management(quantitative_explanatory_variables[:] + categorical_explanatory_variables[:],
-                                   categorical_response_variables[:])
-
-    regression_modeling(quantitative_explanatory_variables, categorical_explanatory_variables,
-                        categorical_response_variables, data)
+    #
+    # # performing data management with data where property is damaged and evaluated clear
+    # data = primary_data_management(quantitative_explanatory_variables[:] + categorical_explanatory_variables[:],
+    #                                categorical_response_variables[:])
+    #
+    # # preforming regression analysis
+    # logistic_regression(quantitative_explanatory_variables, categorical_explanatory_variables,
+    #                     categorical_response_variables, data)
