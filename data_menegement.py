41c03c85e36940420097e1f423ffbbc2bf6f83e6
DmitryBachin <dmitry.bachin@yourapi.io>
2018-09-30 12:01:28 +0200

    data management is commented

--- data_menegement.py
+++ data_menegement.py
@@ -5,9 +5,9 @@ from datetime import datetime, timedelta
 from math import log
 
 
-def damage_property(row,
-                    variable="damage_property"):  # creating the variable damage in int format to avoid 10K 12M 2B etc
-    element = row[variable]
+def damage_property(row):
+    # creating the variable damage in int format to avoid 10K 12M 2B etc
+    element = row["damage_property"]
     multiplier = {
         'K': 10 ** 3,  # for the format like 3.6K
         'M': 10 ** 6,  # for the format like 15M
@@ -29,22 +29,22 @@ def damage_property(row,
         return np.nan
 
 
-def log_count(element):
+def log_count(element):  # logarithm with base 10 and excluding nan
     if element in (0, np.nan):
-        return np.nan
+        return np.nan  # nan and zero are excluded
     else:
         return log(element, 10)
 
 
-def damage_property_lg(row):
+def damage_property_lg(row):  # decrease positive skew
     return log_count(float(row["damage_property"]))
 
 
-def event_duration_lg(row):
+def event_duration_lg(row):  # decrease positive skew
     return log_count(float(row["event_duration"]))
 
 
-def property_damaged(row):
+def property_damaged(row):  # turning damage_property to categorical: whether property was damaged(1) or not (0)
     element = row["damage_property"]
     if element != np.nan:
         return 1 if element > 0 else 0
@@ -52,19 +52,14 @@ def property_damaged(row):
         return np.nan
 
 
-def event_duration(row):
+def event_duration(row):  #
     begin = datetime.strptime(row["begin_date_time"], "%d%b%y:%H:%M:%S")
     end = datetime.strptime(row["end_date_time"], "%d%b%y:%H:%M:%S")
     t = (end - begin).total_seconds() / 3600
     return t
 
 
-def event_category(row):
-    element = row["event_type"]
-    return element
-
-
-def month_name(row):
+def month_name(row):  # adding numerical prefix to month names
     element = row['month_name']
     month_dict = {
         "April": "04",
@@ -84,7 +79,10 @@ def month_name(row):
 
 
 def climate_region(row):
+    # dividing states to climate region
     # based on https://www.ncdc.noaa.gov/monitoring-references/maps/us-climate-regions.php
+    # Alaska is separate from other categories; District of Columbia added to Northeast
+    # As a consequence "Other" is related to events happened near islands or in seas or oceans
     element = row["state"]
     state_to_regions = {'Alabama': 'Southeast',
                         'Alaska': 'Alaska',
@@ -140,54 +138,43 @@ def climate_region(row):
     return state_to_regions.get(element.title(), "Other")
 
 
-def month_name_num(row):
-    element = row['month_name']
-    month_dict = {
-        "April": 4,
-        "August": 8,
-        "December": 12,
-        "February": 2,
-        "January": 1,
-        "July": 7,
-        "June": 6,
-        "March": 3,
-        "May": 5,
-        "November": 11,
-        "October": 10,
-        "September": 9
-    }
-    return month_dict.get(element, 0)
-
-
 def modify_data_set(data_set, variables_to_modify):
+    # takes the variables which should be created or modified
+    # calls the function with the same name as the variable
     for variable in variables_to_modify:
         # modify the variables
         data_set[variable] = data_set.apply(eval(variable), axis=1)
     return data_set
 
 
-def primary_data_management(putative_predictors, response_variables,
+def primary_data_management(explanatory_variables, response_variables,
                             condition="""(data_set["damage_property"] >= 0)"""):
+    # extracts the data set from csv file
+    # modifies
     try:
         # taking the data from the file
         data_set = pandas.read_csv('data_related/storm_event_data.csv', low_memory=False)
     except FileNotFoundError:
         # there is two probable locations of the file, so doesn't make sense to write too many lines about it
         data_set = pandas.read_csv('../data_related/storm_event_data.csv', low_memory=False)
+
+    # in case if I need to display the content, I want to see all columns and rows
     pandas.set_option('display.max_columns', None)
     pandas.set_option('display.max_rows', None)
-    data_set.columns = map(str.lower, data_set.columns)
-    modifiable_variables = retrieve_variables_to_modify()
 
+    # the variables are written in uppercase in the data set, but written in lowercase in the codebook
+    # to be sure I tend to copy variable names from the codebook to the program
+    data_set.columns = map(str.lower, data_set.columns)
+
+    modifiable_variables = retrieve_variables_to_modify()
     data_set = modify_data_set(data_set, modifiable_variables)  # performing necessary modifications of the data set
+
+    # making a subset where we consider only weather events for which damage is evaluated and bigger than or equal zero
+    # the condition can be changed to come other to change the sample
     data_with_damage = data_set[(data_set["damage_property"] >= 0) & eval(condition)]
-    # making a subset where we consider only weather events for which damage is evaluated and bigger than zero
-    return data_with_damage[putative_predictors + response_variables]
+    # because the dataset is huge, to save time, I am leaving only variables with which I work with
+    return data_with_damage[explanatory_variables + response_variables]
 
 
 if __name__ == "__main__":
-    response = retrieve_q_response_variables()
-    explanatory = retrieve_quantitative_explanatory_vars() + retrieve_non_binary_cat_explanatory_vars()
-    data = primary_data_management(explanatory, response, """(data_set["damage_property"] > 0)""")
-    c = data.groupby("damage_property_lg").size()
-    print(c)
+    pass
