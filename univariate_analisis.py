28d669805ef3cee77fa75792161a32036ec627a2
DmitryBachin <dmitry.bachin@yourapi.io>
2018-09-30 11:27:23 +0200

    univariate analysis is finished, and commented

--- univariate_analisis.py
+++ univariate_analisis.py
@@ -4,21 +4,21 @@ import seaborn
 import matplotlib.pyplot as plt
 
 
-def output_distribution(data_set, categorical):  # showing the distribution of the data set by used variables
-    sample_volume = len(data_set)
-    print('The sample volume is %i' % sample_volume)  # printing the sample volume
-    print('')
-    for variable in categorical:
-        print("======================================================================================================")
-        print(f"The {variable} variable distribution")
-        c = data_set.groupby(variable).size()
-        print(c)
-        pt = c * 100 / sample_volume
-        print(f"The percentage {variable} variable distribution")
-        print(pt)
+def output_distribution(data_set, variable,
+                        sample_volume):  # showing the distribution of the data set by used variables
+    # showing the number of events in every category
+    print("======================================================================================================")
+    print(f"The {variable} variable distribution")
+    c = data_set.groupby(variable).size()
+    print(c)
+    # showing the percentage of events in every category
+    pt = c * 100 / sample_volume
+    print(f"The percentage {variable} variable distribution")
+    print(pt)
 
 
 def histogram(variable, data_set):
+    # histogram for categorical variables, shows the number of event in each category
     seaborn.set()
     seaborn.countplot(y=variable, data=data_set)
     plt.ylabel(f"The {variable}")
@@ -26,36 +26,35 @@ def histogram(variable, data_set):
     plt.show()
 
 
-def quantitative_plot(variables, data_set):
-    for variable in variables:
-        seaborn.distplot(data_set[variable].dropna(), kde=False)
-        plt.title(f"The {variable} title")  # adding a title
-        plt.show()
-
-
-def description(variables, data_set):
+def description(variable, data_set):
     # also it is possible to use .mode(), .mean() etc instead of .describe
-    for variable in variables:
-        print(f"Describe {variable}")
-        print(data_set[variable].describe())
+    print(f"Describe {variable}")
+    print(data_set[variable].describe())
+    print(f"The modes are {str(data_set[variable].mode()[1:])}")
 
 
-def univariate_analysis(data_with_damage, cat_explanatory, q_explanaroty, cat_response, q_response):
-    # performs univariate analysis of in
-    all_variables = cat_explanatory[:] + q_explanaroty[:] + cat_response[:] + q_response[:]
-    output_distribution(data_with_damage, cat_explanatory + cat_response)  # showing the distribution in textual format
-    description(all_variables, data_with_damage)
+def univariate_analysis(data_set, cat_explanatory, q_explanatory, cat_response, q_response):
+    print("===========================================================================================================")
+    sample_volume = len(data_set)
+    print('The sample volume is %i' % sample_volume)  # printing the sample volume
+    # performs univariate analysis of all variables
+    all_variables = cat_explanatory[:] + q_explanatory[:] + cat_response[:] + q_response[:]
+
     for variable in all_variables:
-        print("=====================================================================================================")
-        if variable in q_response or variable in q_explanaroty:
-            plt.boxplot(x=data_with_damage[variable])
+        print('=======================================================================================================')
+        description(variable, data_set)  # main features of every variable
+        if variable in q_response or variable in q_explanatory:
+            # showing boxplot and histogram for quantitative variables
+            plt.boxplot(x=data_set[variable])
             plt.title(f"The {variable} box plot")
             plt.show()
-            plt.hist(x=data_with_damage[variable])
+            plt.hist(x=data_set[variable])
             plt.title(f"The {variable} histogram")
             plt.show()
         else:
-            histogram(variable, data_with_damage)
+            output_distribution(data_set, variable, sample_volume)  # showing the distribution in textual format
+            histogram(variable, data_set) # showing histogram for categorical variables
+
 
 
 if __name__ == "__main__":
