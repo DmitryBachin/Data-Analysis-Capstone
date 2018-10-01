87b221cb2fa1b3d797b1bf0259c2a159cdbaaabc
DmitryBachin <dmitry.bachin@yourapi.io>
2018-10-01 19:22:27 +0200

    performed normal way to deal with categorical variables

--- regression_models.py
+++ regression_models.py
@@ -7,10 +7,13 @@ import statsmodels.api as sm
 import statsmodels.formula.api as smf
 
 
-def formula_creation(r_var, q_e_vars, cat_e_vars):
+def formula_creation(r_var, q_e_vars, cat_e_vars, data_set):
     # creating the formula for ols
     formula = f"{r_var} ~ " + " + ".join(q_e_vars)
 
+    for variable in cat_e_vars:
+        first_value = data_set[variable].unique()[0]
+        formula += f" + C({variable}, Treatment(reference='{first_value}'))"
     return formula
 
 
@@ -20,7 +23,7 @@ def regression_modeling(q_explanatory, cat_explanatory, response, data_set):
         data_set[variable] = data_set[variable] - data_set[variable].mean()
 
     for r_var in response:
-        formula = formula_creation(r_var, q_explanatory, cat_explanatory)
+        formula = formula_creation(r_var, q_explanatory, cat_explanatory, data_set)
         print(formula)
         regression_model = smf.ols(formula, data=data_set).fit()
         print(regression_model.summary())
@@ -31,9 +34,7 @@ if __name__ == "__main__":
     quantitative_explanatory_variables = retrieve_quantitative_explanatory_vars()
     categorical_explanatory_variables = retrieve_cat_explanatory_vars()
     quantitative_response_variables = retrieve_q_response_variables()
-    categorical_response_variables = retrieve_cat_response_variables()
 
-    categorical_explanatory_variables.remove("event_type")  # too much categories
 
     # performing data management with data where property is damaged and evaluated clear
     data = primary_data_management(quantitative_explanatory_variables[:] + categorical_explanatory_variables[:],
@@ -43,11 +44,4 @@ if __name__ == "__main__":
     # performing regression analysis
     regression_modeling(quantitative_explanatory_variables[:], categorical_explanatory_variables[:],
                         quantitative_response_variables[:], data)
-    #
-    # # performing data management with data where property is damaged and evaluated clear
-    # data = primary_data_management(quantitative_explanatory_variables[:] + categorical_explanatory_variables[:],
-    #                                categorical_response_variables[:])
-    #
-    # # preforming regression analysis
-    # logistic_regression(quantitative_explanatory_variables, categorical_explanatory_variables,
-    #                     categorical_response_variables, data)
+
