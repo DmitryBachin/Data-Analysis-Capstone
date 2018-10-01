18865451622d28120e53a56fbb7f67035c528b25
DmitryBachin <dmitry.bachin@yourapi.io>
2018-10-01 15:38:40 +0200

    removed one hot encoding

--- regression_models.py
+++ regression_models.py
@@ -12,22 +12,7 @@ def formula_creation(r_var, e_vars):
     return f"{r_var} ~ " + " + ".join(e_vars)
 
 
-def logistic_regression(q_explanatory, cat_explanatory, response, data_set):
-    # lreg = smf.logit(formula = formila, data = data_set).fit()
-    # print(lreg.summary())
-    pass
-
-
 def regression_modeling(q_explanatory, cat_explanatory, response, data_set):
-    # modify categorical variables in one hot encoding
-    if cat_explanatory:
-        modified_data = recode_categorical_variables(data_set, cat_explanatory)
-        other_data = data_set[q_explanatory + response].copy()  # taking non modified variables
-        other_data = other_data.reset_index()  # reset indexing to concat the data frames properly
-        data_set = pd.concat([modified_data, other_data],
-                             axis=1)  # creating new data set where all categorical are binary
-    explanatory_variables = [variable for variable in data_set.columns if variable not in response + ["index"]]
-
     # centering quantitative variables
     for variable in q_explanatory:
         data_set[variable] = data_set[variable] - data_set[variable].mean()
